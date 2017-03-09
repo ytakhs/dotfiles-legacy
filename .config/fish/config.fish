c8649a4b7daf671ab260ea32c402ce1190c62bfd
yuji takahashi <jit.y.31@gmail.com>
2017-03-09 15:08:02 +0900

    Add bobthefish settings not to display date

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,6 @@ set -x PATH $HOME/.nodebrew/current/bin $PATH
 set -x PATH $HOME/.exenv/shims $PATH
 set -x PATH $HOME/.rbenv/shims $PATH
 rbenv init - | source
+
+# bobthefish
+set -g theme_display_date no
