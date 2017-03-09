d139cbe615f7a5cf9410cd784053ada6c7252e2b
yuji takahashi <jit.y.31@gmail.com>
2017-03-09 22:39:59 +0900

    Add setup.sh & Add peco settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,3 +6,23 @@ rbenv init - | source
 # bobthefish
 set -g theme_display_date no
 set -g theme_color_scheme solarized-dark
+
+# peco
+function peco
+  command peco --layout=bottom-up $argv
+end
+function peco-git-checkout
+  git branch -a | peco | tr -d ' ' | read branch
+  echo $branch
+  if [ $branch ]
+    if contains $branch "remotes/"
+      set -l b (echo $branch | awk -F'/' '{print $3}')
+      git checkout -b $b $branch
+    else
+      git checkout $branch
+    end
+  end
+  commandline -f repaint
+end
+
+bind \cx\cr peco-git-checkout
---
+++ setup.sh
@@ -0,0 +1,3 @@
+# fish-shell
+ln -sf $HOME/dotfiles/.config/fish/config.fish $HOME/.config/fish/
+ln -sf $HOME/dotfiles/.config/fish/fishfile $HOME/.config/fish/
