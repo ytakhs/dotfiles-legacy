8b495084a24c1854d7af941036b70290cef9ef91
yuji takahashi <jit.y.31@gmail.com>
2017-05-03 11:28:57 +0900

    Modify config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,39 @@
-set -x PATH $HOME/.nodebrew/current/bin $PATH
-set -x PATH $HOME/.exenv/shims $PATH
-
-if [ -f "$HOME/.asdf/asdf.fish" ]
-  source ~/.asdf/asdf.fish
+# nodebrew
+if [ -d "$HOME/.nodebrew" ]
+  set -x PATH $HOME/.nodebrew/current/bin $PATH
+else
+  echo "nodebrew is not installed"
 end
 
-set -x PATH $HOME/.rbenv/shims $PATH
-rbenv init - | source
+# exenv
+if which exenv > /dev/null 2>&1 ;and [ -d "$HOME/.exenv" ]
+  set -x PATH $HOME/.exenv/shims $PATH
+else
+  echo "exenv is not installed"
+end
+
+# asdf
+if [ -f "$HOME/.asdf/asdf.fish" ]
+  source ~/.asdf/asdf.fish
+else
+  echo "asdf is not installed"
+end
+
+# rbenv
+if which rbenv > /dev/null 2>&1 ;and [ -d "$HOME/.rbenv" ]
+  set -x PATH $HOME/.rbenv/shims $PATH
+  rbenv init - | source
+else
+  echo "rbenv is not installed."
+end
+
+# goenv
+if which goenv > /dev/null 2>&1 ;and [ -d "$HOME/.goenv" ]
+  set -x PATH $HOME/.goenv/shims $PATH
+  goenv init - | source
+else
+  echo "goenv is not installed"
+end
 
 # bobthefish
 set -g theme_display_date no
