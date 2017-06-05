f3de42b0d88635968e5603cff9f0241c6f59e746
yuji takahashi <jit.y.31@gmail.com>
2017-06-05 18:23:11 +0900

    Modify config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,21 +5,21 @@ else
   echo "nodebrew is not installed"
 end
 
-# exenv
+# erlang
+if [ -d ~/erlang/19.3/ ]
+  source ~/erlang/19.3/activate.fish
+else
+  echo "kerl is not installed"
+end
+
+# elixir
 if which exenv > /dev/null 2>&1 ;and [ -d "$HOME/.exenv" ]
   set -x PATH $HOME/.exenv/shims $PATH
 else
   echo "exenv is not installed"
 end
 
-# asdf
-if [ -f "$HOME/.asdf/asdf.fish" ]
-  source ~/.asdf/asdf.fish
-else
-  echo "asdf is not installed"
-end
-
-# rbenv
+# ruby
 if which rbenv > /dev/null 2>&1 ;and [ -d "$HOME/.rbenv" ]
   set -x PATH $HOME/.rbenv/shims $PATH
   rbenv init - | source
@@ -27,15 +27,17 @@ else
   echo "rbenv is not installed."
 end
 
-# goenv
+# go
 if which goenv > /dev/null 2>&1 ;and [ -d "$HOME/.goenv" ]
   set -x PATH $HOME/.goenv/shims $PATH
   goenv init - | source
 else
   echo "goenv is not installed"
 end
+set -x GOPATH $HOME/dev
+set -x PATH $GOPATH/bin $PATH
 
-# opam
+# ocaml
 if which opam > /dev/null 2>&1; and [ -d ~/.opam ]
   source ~/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
   if which rlwrap > /dev/null 2>&1
@@ -54,12 +56,6 @@ if [ -f ~/google-cloud-sdk/path.fish.inc ]
   end;
 else
   echo "google-cloud-sdk is not installed"
-end
-
-if [ -d ~/erlang/19.3/ ]
-  source ~/erlang/19.3/activate.fish
-else
-  echo "kerl is not installed"
 end
 
 # bobthefish
