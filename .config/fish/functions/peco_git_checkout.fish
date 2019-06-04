76227d0594504ace7c3c965c66b55e9db0a42549
YUJI takahashi <jit.y.31@gmail.com>
2019-06-04 20:48:23 +0900

    fish: fix function

--- .config/fish/functions/peco_git_checkout.fish
+++ .config/fish/functions/peco_git_checkout.fish
@@ -2,10 +2,9 @@ function peco_git_checkout
   __fish_disable_bracketed_paste
 
   git branch -a | peco | tr -d ' ' | read branch
-  echo $branch
   if [ $branch ]
     if string match -r "remotes\/" $branch > /dev/null
-      set -l b (string replace -r 'remotes\/(?:google|origin)\/(.*)' '$1' $branch)
+      set -l b (string replace -r 'remotes\/(?:origin)\/(.*)' '$1' $branch)
       git checkout -b $b $branch
     else
       git checkout $branch
