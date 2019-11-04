6907063e34a67ed0b0dc3e61f303889a3080a7d7
YUJI takahashi <jit.y.31@gmail.com>
2019-11-04 16:04:34 +0900

    fish: ruby && rust

--- .config/fish/configs/linux.fish
+++ .config/fish/configs/linux.fish
@@ -1,3 +1,17 @@
 set -x GOPATH $HOME/dev
 set -x PATH $GOPATH/bin $PATH
 set -x EDITOR vim
+
+# rust
+if [ -f $HOME/.cargo/env ]
+  set -Ux fish_user_paths $HOME/.cargo/bin $fish_user_paths
+else
+  echo "no cargo :("
+end
+
+if [ -f $HOME/.rbenv ]
+  set -Ux fish_user_paths $HOME/.rbenv/bin $fish_user_paths
+  status --is-interactive && source (rbenv init -|psub)
+else
+  echo "no rbenv :("
+end
