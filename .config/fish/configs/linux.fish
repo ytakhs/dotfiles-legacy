755f20b99091061d57406703bf68afaa168080ac
YUJI takahashi <jit.y.31@gmail.com>
2020-01-25 12:56:20 +0900

    fish: load brew before at first

--- .config/fish/configs/linux.fish
+++ .config/fish/configs/linux.fish
@@ -2,6 +2,15 @@ set -x GOPATH $HOME/dev
 set -x PATH $GOPATH/bin $PATH
 set -x EDITOR vim
 
+if [ -d /home/linuxbrew/.linuxbrew ]
+  set -x HOMEBREW_PREFIX /home/linuxbrew/.linuxbrew
+  set -x HOMEBREW_CELLAR /home/linuxbrew/.linuxbrew/Cellar
+  set -x HOMEBREW_REPOSITORY /home/linuxbrew/.linuxbrew/Homebrew
+  set -x PATH /home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin $PATH
+  set -x MANPATH /home/linuxbrew/.linuxbrew/share/man $MANPATH
+  set -x INFOPATH /home/linuxbrew/.linuxbrew/share/info $INFOPATH
+end
+
 # rust
 if [ -f $HOME/.cargo/env ]
   set -x fish_user_paths $HOME/.cargo/bin $fish_user_paths
@@ -20,13 +29,4 @@ if [ -d $HOME/dev/opt/flutter ]
   set -x fish_user_paths $HOME/dev/opt/flutter/bin $fish_user_paths
 else
   echo "no flutter :("
-end
-
-if [ -d /home/linuxbrew/.linuxbrew ]
-  set -x HOMEBREW_PREFIX /home/linuxbrew/.linuxbrew
-  set -x HOMEBREW_CELLAR /home/linuxbrew/.linuxbrew/Cellar
-  set -x HOMEBREW_REPOSITORY /home/linuxbrew/.linuxbrew/Homebrew
-  set -x PATH /home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin $PATH
-  set -x MANPATH /home/linuxbrew/.linuxbrew/share/man $MANPATH
-  set -x INFOPATH /home/linuxbrew/.linuxbrew/share/info $INFOPATH
 end
