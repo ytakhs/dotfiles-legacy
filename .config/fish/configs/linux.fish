62937af4e1075ed30d11ecf549a880d0a6bbddec
YUJI takahashi <jit.y.31@gmail.com>
2020-04-12 22:34:44 +0900

    Add DISPLAY for windows x server

--- .config/fish/configs/linux.fish
+++ .config/fish/configs/linux.fish
@@ -2,6 +2,11 @@ set -x GOPATH $HOME/dev
 set -x PATH $GOPATH/bin $PATH
 set -x EDITOR vim
 
+# vcxsrv for WSL2
+if [ -f /etc/resolve.conf ];
+  set -x DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
+end
+
 if [ -d /home/linuxbrew/.linuxbrew ]
   set -x HOMEBREW_PREFIX /home/linuxbrew/.linuxbrew
   set -x HOMEBREW_CELLAR /home/linuxbrew/.linuxbrew/Cellar
