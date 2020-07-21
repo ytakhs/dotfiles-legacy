79ed8cab1f541eef661225416e6f0733084f7f52
YUJI takahashi <jit.y.31@gmail.com>
2020-07-21 20:31:58 +0900

    fish: fix

--- .config/fish/configs/darwin.fish
+++ .config/fish/configs/darwin.fish
@@ -1,26 +1,3 @@
-# elixir
-if which exenv > /dev/null 2>&1 ;and [ -d "$HOME/.exenv" ]
-  set -x PATH $HOME/.exenv/shims $PATH
-else
-  echo "no exenv:("
-end
-
-if [ -d "/opt/elixir" ]
-  set -x PATH /opt/elixir/bin $PATH
-else
-  echo "no elixir :("
-end
-
-# erlang
-if [ -d "/opt/erlang/20.2.1" ]
-  set -x PATH /opt/erlang/20.2.1/bin $PATH
-  if [ -d "$HOME/.cache/rebar3" ]
-    set -x PATH "$HOME/.cache/rebar3/bin" $PATH
-  end
-else
-  echo "no erlang :("
-end
-
 # openssl
 if [ -d "/opt/openssl/1.1.0g" ]
   set -x PATH "/opt/openssl/1.1.0g/bin" $PATH
@@ -50,17 +27,6 @@ else
 end
 set -x GOPATH $HOME/dev
 set -x PATH $GOPATH/bin $PATH
-
-# ocaml
-if which opam > /dev/null 2>&1; and [ -d $HOME/.opam ]
-  source $HOME/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
-  if which rlwrap > /dev/null 2>&1
-    alias ocaml="rlwrap ocaml"
-  end
-else
-  echo "no opam:("
-end
-set -x NVIM_PYTHON_LOG_FILE $HOME/.nvimlog
 
 # python
 # set -x PYENV_ROOT $HOME/.pyenv
